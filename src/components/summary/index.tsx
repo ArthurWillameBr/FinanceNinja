0d4a8920da4800bdbd2abc479b5e5360103ffddb
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-20 15:17:25 -0300

    feat: add Summary Component

---
+++ src/components/summary/index.tsx
@@ -0,0 +1,30 @@
+import { ArrowCircleDown, ArrowCircleUp, Money } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00B37E" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <Money size={32} color="#00B37E" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+}
