e5576712db5f168f65a93d8bed6daa935a84e777
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-20 15:29:44 -0300

    feat: add TransactionsTable component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,9 +1,39 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
-    return(
-        <div>
-            <Header/>
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+
+              <td>Venda</td>
+              <td>20/05/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>20/05/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </>
+  );
 }
