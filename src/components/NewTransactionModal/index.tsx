f5b7bfc8539be2f941c1916628d9965212fdf787
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-21 12:43:58 -0300

    feat: ensure selection when clicking buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,11 +22,11 @@ export function NewTransactionModal() {
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
           <TransactionType>
-            <TransactionTypeButton variant="income">
+            <TransactionTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
-            <TransactionTypeButton variant="outcome">
+            <TransactionTypeButton variant="outcome" value="outcome">
               <ArrowCircleDown size={24} />
               Saida
             </TransactionTypeButton>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
+
 
 export const Overlay = styled(Dialog.Overlay)`
   width: 100vw;
@@ -67,7 +69,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme["gray-500"]};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -78,7 +80,7 @@ interface TransactionTypeButtonProps {
   variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -95,5 +97,18 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
       props.variant === "income"
         ? props.theme["green-500"]
         : props.theme["red-500"]};
+  }
+
+  &[data-state="unchecked"]:hover {
+    background: ${(props) => props.theme["gray-600"]};
+  } 
+
+  &[data-state="checked"] {
+    color: ${(props) => props.theme.white};
+    background: ${props => props.variant === "income" ? props.theme["green-500"] : props.theme["red-500"]};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `;
