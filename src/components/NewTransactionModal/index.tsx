29a460a420e2ec385fa4da6f661917c93e5e9977
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-21 19:16:44 -0300

    feat: ensure receipt of form data with react hook form and zod

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -7,8 +7,28 @@ import {
   TransactionTypeButton,
 } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const NewTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(["income", "outcome"]),
+});
+
+type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const { register, handleSubmit, formState: {isSubmitting} } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(NewTransactionFormSchema),
+  });
+
+  function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -17,10 +37,25 @@ export function NewTransactionModal() {
           <X size={24} />
         </CloseButton>
         <Dialog.Title>Nova transação</Dialog.Title>
-        <form action="">
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register("description")}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register("price", {valueAsNumber: true})}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register("category")}
+          />
           <TransactionType>
             <TransactionTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
@@ -32,7 +67,7 @@ export function NewTransactionModal() {
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type="submit">Cadastrar</button>
+          <button type="submit" disabled={isSubmitting}>Cadastrar</button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/pages/transactions/components/SearchForm/index.tsx
+++ src/pages/transactions/components/SearchForm/index.tsx
@@ -1,14 +1,36 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const searchFormSchema = z.object({
+  query: z.string(),
+
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { register, handleSubmit, formState: {isSubmitting} } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  });
+
+  function handleSearchTransactions(data: SearchFormInputs) {
+      console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
-      <button type="submit">
-        <MagnifyingGlass size={20}/>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input type="text" 
+      placeholder="Busque por transações" 
+      {...register('query')} 
+      />
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
         Buscar
-        </button>
+      </button>
     </SearchFormContainer>
   );
 }
