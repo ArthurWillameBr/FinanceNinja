a3bbea35f5ab61225c4f0718f0170555bdd18077
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-21 12:29:09 -0300

    feat: add income and outcome buttons to modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
-import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import * as Dialog from "@radix-ui/react-dialog";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -8,13 +14,24 @@ export function NewTransactionModal() {
       <Overlay />
       <Content>
         <CloseButton>
-            <X size={24}/>
+          <X size={24} />
         </CloseButton>
         <Dialog.Title>Nova transação</Dialog.Title>
         <form action="">
-         <input type="text" placeholder="Descrição" required/>
-          <input type="number" placeholder="Preço" required/>
-          <input type="text" placeholder="Categoria" required/>
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">Cadastrar</button>
         </form>
       </Content>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -66,3 +66,34 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   color: ${(props) => props.theme["gray-500"]};
 `;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: "income" | "outcome";
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  gap: 0.5rem;
+  border: 0;
+  color: ${(props) => props.theme["gray-300"]};
+  cursor: pointer;
+
+  svg {
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
+  }
+`;
